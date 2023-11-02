4c34e810061ad8df57091b18039462be6b6a659c
David Hammond <dhammond@economicsandpeace.org>
2023-11-02 15:04:54 +1100

    parallel

--- R/02-git_schedule.R
+++ R/02-git_schedule.R
@@ -9,6 +9,7 @@
 #' @importFrom dplyr filter relocate select mutate rename as_tibble n
 #' @importFrom tidyr spread unnest
 #' @importFrom jsonlite fromJSON
+#' @importFrom parallel makeCluster detectCores stopCluster parLapply
 #'
 #'
 #' @return Projects
@@ -19,11 +20,12 @@ git_schedule = function(token, org){
   projects = git_projects(token, org)  %>%
     filter(!template, !closed)
   all = NULL
-  for (i in 1:nrow(projects)){
-    print(i)
-    res = git_issues(projects[i,])
-    all = all %>% rbind(res)
-  }
+  projects = split(projects, projects$id)
+  cores <- max(c(1, detectCores()-1))
+  cl <- makeCluster(cores)
+  all = parLapply(cl, projects, git_issues)
+  stopCluster(cl)
+  all = all %>% bind_rows()
   all = all %>% mutate(Due = as.Date(Due))
   return(all)
 }
